763190e4154da04d1e7e82a4cfa7f08994f30019
Vu Nguyen <vu.nguyen101@gmail.com>
2021-02-11 12:01:56 +0200

    set up blog model

---
+++ models/blog.js
@@ -0,0 +1,17 @@
+import mongoose from 'mongoose'
+
+const blogSchema = new mongoose.Schema({
+	title: String,
+	author: String,
+	url: String,
+	likes: Number,
+})
+
+blogSchema.set('toJSON', {
+	transform: (doc, returnedObj) => {
+		;(returnedObj.id = returnedObj._id), delete returnedObj._id
+		delete returnedObj.__v
+	},
+})
+
+export default mongoose.model('Blog', blogSchema)
