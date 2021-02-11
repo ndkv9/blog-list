4ea7c5fde21eba00a9642e128dbadc02c13a6269
Vu Nguyen <vu.nguyen101@gmail.com>
2021-02-11 11:52:34 +0200

    add env

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 import http from 'http'
 import app from './app'
+import config from './utils/config'
+import logger from './utils/logger'
 
 const server = http.createServer(app)
 
-server.listen()
+server.listen(config.PORT, () => {
+	logger.info(`server running on port ${config.PORT}`)
+})
