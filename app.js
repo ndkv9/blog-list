e44815cae15c442470438090b967ca66213f70ec
Vu Nguyen <vu.nguyen101@gmail.com>
2021-02-11 12:21:37 +0200

    connect to DB

--- app.js
+++ app.js
@@ -5,9 +5,14 @@ import config from './utils/config'
 import logger from './utils/logger'
 import cors from 'cors'
 import middleware from './utils/middleware'
+import blogRouter from './controllers/blogs'
+import Blog from './models/blog'
 
 app.use(cors())
 app.use(express.json())
+app.use('/api/blogs', blogRouter)
+
+Blog.connect(config.MONGODB_URI)
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
