4d99ad65ca5a70f3d29061169a3e82e6627c44ab
Vu Nguyen <vu.nguyen101@gmail.com>
2021-02-11 12:18:59 +0200

    setup blogRouter

--- app.js
+++ app.js
@@ -4,7 +4,12 @@ const app = express()
 import config from './utils/config'
 import logger from './utils/logger'
 import cors from 'cors'
+import middleware from './utils/middleware'
 
 app.use(cors())
+app.use(express.json())
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
 
 export default app
---
+++ controllers/blogs.js
@@ -0,0 +1,23 @@
+import express from 'express'
+import Blog from './model/blog'
+
+const blogRouter = express.Router()
+
+blogRouter.get('/', (req, res, next) => {
+	Blog.find({})
+		.then(blogs => blogs.toJSON())
+		.then(formattedBlogs => res.json(formattedBlogs))
+		.catch(error => next(error))
+})
+
+blogRouter.post('/', (req, res, next) => {
+	const blog = new Blog(req.body)
+
+	blog
+		.save()
+		.then(blog => blog.toJSON())
+		.then(formattedBlog => res.status(201).json(formattedBlog))
+		.catch(error => next(error))
+})
+
+export default blogRouter
