46eaf00d6fadf14fb64c91670015f147c95e8db2
Vu Nguyen <vu.nguyen101@gmail.com>
2021-02-15 10:50:22 +0200

    add totalLikes fn

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -2,6 +2,11 @@ const dummy = blogs => {
 	return 1
 }
 
+const totalLikes = blogs => {
+	return blogs.map(b => b.likes).reduce((a, i) => a + i, 0)
+}
+
 module.exports = {
 	dummy,
+	totalLikes,
 }
