5267651efc75d1c03a18abfa9fe3d2d309ba984b
Vu Nguyen <vu.nguyen101@gmail.com>
2021-02-15 13:13:21 +0200

    add favorite blog test cases

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -8,7 +8,14 @@ const totalLikes = blogs => {
 		: blogs.map(b => b.likes).reduce((a, i) => a + i, 0)
 }
 
+const favoriteBlog = blogs => {
+	return blogs.length === 0
+		? null
+		: blogs.find(b => b.likes === Math.max(...blogs.map(b => b.likes)))
+}
+
 module.exports = {
 	dummy,
 	totalLikes,
+	favoriteBlog,
 }
