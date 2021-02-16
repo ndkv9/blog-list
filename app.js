a43e5d69db78c3de699ff682442b0c2cf12760d3
Vu Nguyen <vu.nguyen101@gmail.com>
2021-02-16 11:13:01 +0200

    install helmet

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const cors = require('cors')
 const middleware = require('./utils/middleware.js')
 const blogRouter = require('./controllers/blogs.js')
 const mongoose = require('mongoose')
+const helmet = require('helmet')
 
 mongoose
 	.connect(config.MONGODB_URI, {
@@ -23,6 +24,7 @@ mongoose
 	})
 
 app.use(cors())
+app.use(helmet())
 app.use(express.json())
 app.use('/api/blogs', blogRouter)
 
