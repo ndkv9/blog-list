581d95619329b8d181f72f28f6080c81de93e47d
Vu Nguyen <vu.nguyen101@gmail.com>
2021-02-18 16:00:59 +0200

    modify adding new blog route with token

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,8 +1,19 @@
 const express = require('express')
 const Blog = require('../models/blog.js')
 const User = require('../models/user.js')
+const jwt = require('jsonwebtoken')
 
 const blogsRouter = express.Router()
+
+// helper function to get token from request
+const getToken = request => {
+	const authorization = request.get('authorization')
+	if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+		return authorization.slice(7)
+	}
+
+	return null
+}
 
 blogsRouter.get('/', async (req, res) => {
 	const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
@@ -10,15 +21,23 @@ blogsRouter.get('/', async (req, res) => {
 })
 
 blogsRouter.post('/', async (req, res) => {
+	const token = await getToken(req)
+	const decodedToken = jwt.verify(token, process.env.SECRET)
+
+	if (!token || !decodedToken.id) {
+		return res.status(401).json({ error: 'invalid token' })
+	}
+
+	const user = await User.findById(decodedToken.id)
+
 	const blog = new Blog(req.body)
+
 	blog.likes ? blog.likes : (blog.likes = 0)
 
 	if (!blog.url || !blog.title) {
 		res.status(400).json({ error: 'title or url missing!' })
 	}
 	const newBlog = await blog.save()
-
-	const user = await User.findById(blog.user)
 	user.blogs = user.blogs.concat(newBlog._id)
 	await user.save()
 
--- utils/middleware.js
+++ utils/middleware.js
@@ -8,9 +8,11 @@ const errorHandler = (error, req, res, next) => {
 	logger.error(error)
 
 	if (error.name === 'CastError') {
-		return response.status(400).send({ error: 'malformatted id' })
+		return res.status(400).send({ error: 'malformatted id' })
 	} else if (error.name === 'ValidationError') {
-		return response.status(400).json({ error: error.message })
+		return res.status(400).json({ error: error.message })
+	} else if (error.name === 'JsonWebTokenError') {
+		return res.status(400).json({ error: 'invalid token' })
 	}
 
 	next(error)
